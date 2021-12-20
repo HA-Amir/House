814ef89cfef60ebc7063b6087dfccbef4a691084
HA-Amir <you@example.com>
2021-12-20 04:42:58 +0100

    first commit

--- server/config/database.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    database:'mongodb+srv://root:root@auction-house.izmbz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' || "mongodb://localhost:27017/auction",
-    secret: "mysecret"
-}
--- server/database-mongodb/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const config= require('../config/database')
-mongoose.connect(config.database, () => {
-  console.log("db connected");
-});
-const db = mongoose.connection;
-
-module.exports = db;
